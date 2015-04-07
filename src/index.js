bbac2329b66f5e74d04f94975f7edc900019ad7f
Joel Olsson <joel.olsson@bullioncapital.com>
2015-04-07 18:29:49 +1000

    Written tests and got base functionality of error object down

--- src/index.js
+++ src/index.js
@@ -1,8 +1,86 @@
-function validateErrorInput(errObj){
+function validProperty(prop, errObj){
+	var validProps = ['type', 'message', 'title', 'statusCode', 'validationCode', 'data'];
+	return validProps.some(function(item){
+		return prop === item && (!errObj || Object.prototype.hasOwnProperty.call(errObj, prop)) ;
+	});
+}
 
+function validProperties(errObj, callback){
+	var atLeastOneValid = false;
+
+	for (var prop in errObj){
+		if ( validProperty(prop, errObj) ){
+			atLeastOneValid = true;
+
+			if (callback){
+				callback(prop, errObj);
+			}
+		}
+	}
+	
+	return atLeastOneValid;
+}
+
+function validErrorInput(passedErr){
+	"use strict";
+	var paramType = typeof passedErr;
+
+	// Allow object literal with at least one valid property or a string. 
+	if (paramType === 'object' && !Array.isArray(passedErr) && passedErr !== null){
+		return validProperties(passedErr);
+	} else if (paramType === 'string' && passedErr.trim().length){
+		return true;
+	}
+	return false;
+}
+
+function ErrorConstructor(passedErr){
+	if (!validErrorInput(passedErr)){
+		this.properties = false;
+
+	} else if (typeof passedErr === 'string') {
+		this.properties = passedErr;
+
+	} else {
+		this.properties = {};
+
+		validProperties(passedErr, function(prop, errObj){
+			this.properties[prop] = errObj[prop];
+		}.bind(this));
+	}
+} 
+
+ErrorConstructor.prototype.returnMessage = function(msgType, callback){
+	if (!this.properties){
+		return 'Incorrect Error Type';
+
+	} else if (typeof this.properties === 'string'){
+		return this.properties;
+
+	} else if (!Object.prototype.hasOwnProperty.call(this.properties, msgType)){
+		return msgType + ' field not provided to the errorHandler';
+
+	} else {
+		return callback();
+	}
+};
+
+
+ErrorConstructor.prototype.getType = function(){
+	return this.returnMessage('type', function(){
+		return this.properties.type;
+	}.bind(this));
+};
+
+function errorHandler(passedErr){
+	var errHandler = new ErrorConstructor(passedErr);
+	return errHandler;
 }
 
 
 module.exports = {
-	validateErrorInput : validateErrorInput,
+	validProperty : validProperty,
+	validProperties : validProperties,
+	validErrorInput : validErrorInput,
+	errorHandler: errorHandler
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,77 @@
 var test = require('tape');
 var errHandler = require('../src/index');
 
-test('Valid error input', function(t){
+test('Valid property function', function(t){
+	t.plan(2);
+	//should return true for valid properties and false for invalid props.
+	t.equal(errHandler.validProperty('message'),  true);
+	t.equal(errHandler.validProperty('meefewfefwe'),  false);
+});
+
+test('Valid properties', function(t){
 	t.plan(2);
 
-	//
-	t.equal(errHandler.validateErrorInput(null),  false);
+	//should return true if there is at least one valid property and false if there are no valid props.
+	t.equal(errHandler.validProperties({message:'test'}),  true);
+	t.equal(errHandler.validProperties({mefwefwef: 'test'}),  false);
+});
+
+
+test('Valid error input', function(t){
+	t.plan(9);
+
+	//Check Invalid Error Format - Must not contain null, undefined, numbers, arrays, empty objects, 
+	//objects with invalid properties or empty strings.
+
+	t.equal(errHandler.validErrorInput(null),  false);
+
+	t.equal(errHandler.validErrorInput(),  false);
+
+	t.equal(errHandler.validErrorInput(8),  false);
+
+	t.equal(errHandler.validErrorInput([]),  false);
+
+	t.equal(errHandler.validErrorInput({}),  false);
+
+	t.equal(errHandler.validErrorInput(""),  false);
+
+	t.equal(errHandler.validErrorInput(" "),  false);
+
+
+	//Approved Valid Error Format String - Objects must contain appropriate 
+	//properties and strings must have at least one character
+
+	t.equal(errHandler.validErrorInput({type: "string" }),  true);
+
+	t.equal(errHandler.validErrorInput("string"),  true);
+});
+
+
+test('Instantiates and returns a custom error object', function(t){
+	t.plan(1);
+
+	var errObj = errHandler.errorHandler();	
+	//Should return an error object
+	t.equal(typeof errObj,  'object');
+});
+
+test('Determine error properties are added', function(t){
+	t.plan(4);
+
+	var errObj = errHandler.errorHandler({type:"test", message:"random message", title:"randTitle", randomProp:"testing"});	
+	//Should return an error object
+	t.equal(errObj.properties.type,  "test");
+	t.equal(errObj.properties.message,  "random message");
+	t.equal(errObj.properties.title,  "randTitle");
+
+	//Should not return invalid properties
+	t.equal(errObj.properties.randomProp,  undefined);
+});
+
+test('Determine error methods are working', function(t){
+	t.plan(1);
+
+	var errObj = errHandler.errorHandler({type:"test", message:"random message", title:"randTitle", randomProp:"testing"});	
+	//Should return an error object
+	t.equal(errObj.getType(),  "test");
 });
